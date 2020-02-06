0243e212ab2580b195c0c13e12fa8851d9eb6d36
ajbdev <andybaird@gmail.com>
2020-02-05 22:42:13 -0700

    Preliminary parser work

---
+++ README.md
@@ -0,0 +1,58 @@
+# Heroes Changelog
+
+This module parses Heroes of the Storm patch notes and categories changes by Hero. 
+
+This module relies entirely on the fantastic [heroes-talents](https://github.com/heroespatchnotes/heroes-talents) and [heroes-patch-data](https://github.com/heroespatchnotes/heroes-patch-data) repos. If these repos are not up to date this module will not parse patch notes for new patches or heroes.
+
+## Usage
+
+```javascript
+
+const heroeschangelog = require('heroeschangelog');
+
+const { patches, heroes } = heroeschangelog();
+
+console.log(patches);
+
+
+ [{ 
+      "internalId":"patch-notes-february-14-2017",
+      "patchName":"Lucio Patch",
+      "officialLink":"http://us.battle.net/heroes/en/blog/20554028",
+      "patchType":"Patch Notes",
+      "gameVersion":"23.3",
+      "fullVersion":"1.23.3.50441",
+      "ptrOfficialLink":"http://us.battle.net/heroes/en/blog/20529510",
+      "ptrDate":"2017-02-06",
+      "ptrBuild":"50424",
+      "liveDate":"2017-02-14",
+      "liveBuild":"50441",
+      "fullNotesMarkdown":"(...full patch notes converted to markdown)",
+      "changedHeroes": ['Abathur','Varian','Jaina','Zarya']
+}...]
+
+
+console.log(heroes.Artanis.changes)
+
+[{
+    "2.28.3.58623": { 
+        patch: { ...patch object },
+        changes: "* Health reduced from 2335 (+4% per level) to 2245 (+4% per level).
+    * Health Regeneration reduced from approximately 4.86 (+4% per level) to 4.67 (+4% per level).
+        * Twin Blades (W)
+            * Artanis’ next Basic Attack now also causes him to charge forward a short distance, equal to approximately half the distance provided by the previous iteration of the Zealot Charge Talent.
+            * Zealot Charge (Talent)
+                * Now increases charge distance by 100%.
+            
+Developer Comments: Artanis’ survivability directly scales with how much uptime he has, which made a Talent like Zealot Charge incredibly powerful. The effect brought his entire kit together so well, that we’ve decided to provide part of its former benefit into baseline Twin Blades. Whereas sometimes we feel the need to cut Talents to fill a hole in a Hero’s kit, in this case we think that this is a hole that Artanis’ should be able to partially circumvent from the start. Since this is a substantial increase in his power at the start of the game, we’ve decided to revert the Health increase that Artanis received recently."
+    }
+}]
+
+```
+
+### Caveats
+
+* Only items under the specific hero header and the "bug fixes" header are categorized as a change for a hero. If there is a change to a hero mentioned elsewhere in the patch notes, the change will not get parsed. 
+* Certain patch notes are not parsed because they are not formatted uniformly (mostly older patches that have no revelant changes)
+* Patch notes released on the HotS forums are not parsed. These changes are usually game breaking bug hotfixes and usually don't have hero changes.
+* The object graph of these generated objects is very large. Be warned if you intend on serving these across an HTTP connection or saving them out. 
---
+++ index.js
@@ -0,0 +1,112 @@
+const patches = require('heroes-patch-data');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const heroestalents = require('heroes-talents');
+const fs = require('fs');
+const turndown = require('turndown');
+const td = new turndown();
+
+td.addRule('remove-images', {
+    filter: ['img'],
+    replacement: (content) => ''
+});
+
+
+
+let heroes = {};
+
+async function scrapePatchNotes(patch) {
+    const response = await axios(patch.officialLink);
+    const $ = cheerio.load(response.data);
+
+    const patchRootContainer = (patch.officialLink.indexOf('blizztrack.com') > -1) ? '.patch-notes' : '.article-container';
+
+    const articleHtml = $(patchRootContainer).html();
+
+    if (!articleHtml) {
+        console.log(`Skipping ${patch.patchName} [${patch.officialLink}] - ${patch.liveDate} - no article body font`);
+        return;
+    }
+
+    patch.fullNotesMarkdown = td.turndown(articleHtml);
+
+    patch.changedHeroes =  [];
+
+    $('.article-container h4').each((i,el) => {
+        const title = $(el).text().trim();
+
+        if (heroes[title]) {
+            let sibling = el.next;
+
+            // Select all next siblings 
+            const selections = [];
+            while (sibling && ['h1','h2','h3','h4','h5'].indexOf(sibling.name) === -1) {
+                if ($(sibling).text().trim()==='Return' || sibling.name === 'img') {
+                    // Don't capture return links or images
+                    sibling = sibling.next;
+                    continue;
+                }
+                selections.push(sibling);
+                sibling = sibling.next;
+            }
+
+            const changesAsMarkdown = td.turndown(cheerio.html(selections));
+
+            heroes[title].changes[patch.fullVersion] = { patch: patch, changes: changesAsMarkdown};
+
+            patch.changedHeroes.push(title);
+        }
+    });
+
+    $('.article-container h3').each((i, el) => {
+        if ($(el).text() === 'Bug Fixes') {
+
+            sibling = el.next;
+            const selections = [];
+            while (sibling) {
+                selections.push(sibling);
+                sibling = sibling.next;
+            }
+
+            const bugfixes = $(selections).text().split('\n');
+
+            Object.keys(heroes).forEach((hero) => {
+                bugfixes.forEach((bugfix, i) => {
+                    if (bugfix.toUpperCase().indexOf(hero.toUpperCase()) > -1) {
+                        if (!heroes[hero].changes[patch.fullVersion]) {
+                            heroes[hero].changes[patch.fullVersion] = { patch: patch, changes: ''} ;
+                        }
+                        
+                        heroes[hero].changes[patch.fullVersion].changes += '\nBug fix: ' + bugfix.trim();
+
+                        patch.changedHeroes.push(hero);
+                    }
+                });
+            });        
+        }
+    });
+
+    patch.changedHeroes = patch.changedHeroes((v, i, s) => s.indexOf(v) === i);
+
+    console.log(`Parsed ${patch.patchName} [${patch.officialLink}] - ${patch.liveDate} - Changed Heroes: ${Array.from(patch.changedHeroes).join(', ')}`);
+}
+
+async function iteratePatches(patches) {
+
+    heroes = await heroestalents.loadHeroJSONFiles();
+
+    Object.keys(heroes).map((h) => heroes[h].changes = {});
+
+    for (patch of patches) {
+        if (patch.officialLink) {
+            await scrapePatchNotes(patch, heroes);
+        }
+    }
+
+    return {
+        patches: patches,
+        heroes: heroes
+    }
+}
+
+module.exports = () => iteratePatches(patches);
